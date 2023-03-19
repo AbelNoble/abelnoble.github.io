5671743aedd1cffa1f57623ba7f47783206d5479
Abel Noble <abel.noble3@gmail.com>
2023-03-18 23:20:16 -0400

    more changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 /*!
  * Webflow: Front-end site library
  * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
  */ !(function (t) {
   var e = {};
   function n(i) {
